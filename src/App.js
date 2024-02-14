68e8e95bea1389cc5e5b0c87349947b08cac66f9
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2024-02-14 16:02:39 +0400

    add task1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          <tr>
+            <td>Фамилия</td>
+            <td>Цуканов</td>
+          </tr>
+          <tr>
+            <td>Имя</td>
+            <td>Денис</td>
+          </tr>
+          <tr>
+            <td>Почта</td>
+            <td>tsukanovdenis83@yandex.ru</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
